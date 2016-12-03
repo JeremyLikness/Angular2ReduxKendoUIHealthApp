73503706bb028d3573d911c982dc0c03b05efee1
Jeremy Likness <jeremy@jeremylikness.com>
2016-12-03 12:08:37 -0500

    fix immutability of the store in the reducers

--- src/app/state/reducer.ts
+++ src/app/state/reducer.ts
@@ -7,34 +7,37 @@ import { IAction,
     Actions } from './actions';
 
 export function statReducer<T extends IAction>(state: Stats, action: T) {
+
+    let newState = new Stats();
+
     switch (action.type) {
 
         case Actions.ChangeAge:
             let ageAction = <IChangeAgeAction><any>action;
-            let newStat = Object.assign(state, { ageYears: ageAction.age });
+            let newStat = Object.assign(newState, state, { ageYears: ageAction.age });
             newStat.bmrHistory = [...state.bmrHistory, newStat.bmr];
             return newStat;
 
         case Actions.ChangeHeight:
             let heightAction = <IChangeHeightAction><any>action;
-            let newHeightStat = Object.assign(state, { heightInches: heightAction.height });
+            let newHeightStat = Object.assign(newState, state, { heightInches: heightAction.height });
             newHeightStat.bmrHistory = [...state.bmrHistory, newHeightStat.bmr];
             return newHeightStat;
 
         case Actions.ChangeWeight:
             let weightAction = <IChangeWeightAction><any>action;
-            let newWeightStat = Object.assign(state, { weightPounds: weightAction.weight });
+            let newWeightStat = Object.assign(newState, state, { weightPounds: weightAction.weight });
             newWeightStat.bmrHistory = [...state.bmrHistory, newWeightStat.bmr];
             return newWeightStat;
 
         case Actions.ChangeGender:
             let genderAction = <IChangeGenderAction><any>action;
-            let newGenderStat = Object.assign(state, { isFemale: genderAction.isFemale });
+            let newGenderStat = Object.assign(newState, state, { isFemale: genderAction.isFemale });
             newGenderStat.bmrHistory = [...state.bmrHistory, newGenderStat.bmr];
             return newGenderStat;
 
         default: {
-            return Object.assign(new Stats(), DEFAULT_STAT);
+            return Object.assign(newState, DEFAULT_STAT);
         }
     }
 };
